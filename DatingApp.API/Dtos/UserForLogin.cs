4cba9fb357a2ddf197c91ebc1dcc548774bd4b2b
bahartutul <emrul.bahar@synesisit.com.bd>
2019-04-10 17:38:04 +0600

    Token Commit

---
+++ DatingApp.API/Dtos/UserForLogin.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Dtos
+{
+    public class UserForLogin
+    {
+         
+        public string username { get; set; }
+       
+        public string password{get;set;}
+    }
+}
--- DatingApp.API/Dtos/UserForRegister.cs
+++ DatingApp.API/Dtos/UserForRegister.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegister
     {
+        [Required]
         public string username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage="Must be 4 to 8 character")]
         public string password{get;set;}
     }
 }
